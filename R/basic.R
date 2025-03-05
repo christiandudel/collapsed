35d5b2aa41810aec8fdf9ca9c05a72b1ad45e36d
Christian Dudel <dudel@demogr.mpg.de>
2025-03-05 18:28:29 +0100

    Added basic simulation script and placeholder readme

---
+++ R/basic.R
@@ -0,0 +1,84 @@
+### Collapsing predictors, proof of concept
+
+# Seed
+set.seed(1932)
+
+# Outcome model
+constant <- 0
+positive <- 2
+negative <- -2
+errorsd <- 0.5
+
+# Probability of observing selected studies
+startpositive <- 0.4 
+endpositive <- 0.05
+
+startnegative <- 0.1
+endnegative <- 0.2
+
+# Years
+years <- 1970:2020
+nyears <- length(years)
+
+# Studies per year
+n <- 10
+
+# Interpolating probabilities
+positiveprobs <- nyears
+negativeprobs <- nyears
+neutralprobs <- nyears
+
+positiveprobs <- seq(startpositive,endpositive,length.out=nyears)
+negativeprobs <- seq(startnegative,endnegative,length.out=nyears)
+neutralprobs <- 1-(positiveprobs+negativeprobs)
+
+# Generate data frame
+simulateddata <- data.frame(year=numeric(),
+                            positive=numeric(),
+                            negative=numeric(),
+                            neutral=numeric(),
+                            outcome=numeric())
+# Number of studies
+nostudies <- n*nyears
+
+# Loop over years
+for(year in years) {
+  whichyear <- which(years==year)
+  yearvar <- rep(year,n)
+  typevar <- sample(c(1,2,3),size=n,rep=TRUE,prob=c(positiveprobs[whichyear],
+                                                    negativeprobs[whichyear],
+                                                    neutralprobs[whichyear]))
+  
+  positivevar <- as.numeric(typevar==1)
+  negativevar <- as.numeric(typevar==2)
+  neutralvar <- as.numeric(typevar==3)
+  
+  outcomevar <- constant + positivevar*positive + negativevar*negative + rnorm(n=n,sd=errorsd)
+  
+  tmpdata <- data.frame(year=year,
+                        positive=positivevar,
+                        negative=negativevar,
+                        neutral=neutralvar,
+                        outcome=outcomevar)
+  
+  simulateddata <- rbind(simulateddata,tmpdata)
+  
+}
+
+# Dummy for selection
+simulateddata$selected <- as.numeric(simulateddata$positive==1|simulateddata$negative==1)
+
+# Regressions
+fit1 <- lm(outcome~year,data=simulateddata)
+fit2 <- lm(outcome~year+selected,data=simulateddata)
+fit3 <- lm(outcome~year+positive+negative,data=simulateddata)
+
+# Results: overview
+summary(fit1)
+summary(fit2)
+summary(fit3)
+
+# Effect of going from first to last year, standardized
+coef(fit1)["year"]*nyears/sd(simulateddata$outcome)
+coef(fit2)["year"]*nyears/sd(simulateddata$outcome)
+coef(fit3)["year"]*nyears/sd(simulateddata$outcome)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# collapsed
-Simple simulation script to assess the impact of collapsing dummy predictors of opposite signs
+# Collapsing binary predictors
+
+Christian Dudel, dudel@demogr.mpg.de
+
+Simple simulation script to assess the impact of collapsing dummy predictors of 
+opposite signs into one dummy. Proof of concept.
