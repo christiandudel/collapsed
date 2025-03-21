a3fb804992d09e2dc24e3d9df4055dfbbc56a7c4
Christian Dudel <dudel@demogr.mpg.de>
2025-03-21 15:16:34 +0100

    Added new plausibility checks

--- R/eyeballing.R
+++ R/eyeballing.R
@@ -1,31 +1,78 @@
-# Norway at age 45, cohort 1935 vs 1973 
+### Values for childlessness 
+# Norwegian men at age 45, cohort 1935 vs 1973 
 # eyeballed from Figure 1 in Kravdal https://doi.org/10.1007/s10680-021-09590-4
 
-childless1 <- 0.15
-childless2 <- 0.25
+childless1 <- 0.15 # Cohort 1935
+childless2 <- 0.25 # Cohort 1973
 child1 <- 1-childless1
 child2 <- 1-childless2
 
-# https://doi.org/10.1093/humupd/dmac035, Table 1, Sperm concentration (upper part)
-average1 <- 83.5 # 1973
-average2 <- 57.1 # 2018
+### Sperm concentration
+# https://doi.org/10.1093/humupd/dmac035
+# Table 1, upper part, unselected/fertile from stratified model
+total_sc_1 <- 83.5 # 1973
+total_sc_2 <- 57.1 # 2018
 
-unselected1 <- 101.2 # 1973
-unselected2 <- 49.0 # 2018
+unselected_sc_1 <- 101.2 # 1973; these are the numbers are also used in the graphical abstract
+unselected_sc_2 <- 49.0 # 2018
 
-fertile1 <- 77.3 # 1977
-fertile2 <- 72.8 # 2017
+fertile_sc_1 <- 77.3 # 1977
+fertile_sc_2 <- 72.8 # 2017
 
-### Working with the overall average
-opposite1a <- (average1-fertile1*child1)/childless1
-opposite2a <- (average2-fertile2*child2)/childless2
+# Reverse calculation with total
+infertile_sc_t_1 <- (total_sc_1-fertile_sc_1*child1)/childless1
+infertile_sc_t_2 <- (total_sc_2-fertile_sc_2*child2)/childless2
 
-1-opposite2a/opposite1a
-1-average2/average1
+# Reverse calculation comes from
+# total = fertile_sc * proportion_fertile + infertile_sc * proportion_infertile
+# rearranging gives
+# infertile_sc = (total - fertile_sc * proportion_fertile)/proportion_infertile
 
-### Working with unselected
-opposite1u <- (unselected1-fertile1*child1)/childless1
-opposite2u <- (unselected2-fertile2*child2)/childless2
+# Reverse calculation with unselected
+infertile_sc_u_1 <- (unselected_sc_1-fertile_sc_1*child1)/childless1
+infertile_sc_u_2 <- (unselected_sc_2-fertile_sc_2*child2)/childless2
 
-1-opposite2u/opposite1u
-1-unselected2/unselected1
+### Total sperm count
+# https://doi.org/10.1093/humupd/dmac035
+# Table 1, lower part, unselected/fertile from stratified model
+total_tsc_1 <- 297.4 # 1973
+total_tsc_2 <- 205.6 # 2018
+
+unselected_tsc_1 <- 335.7 # 1973
+unselected_tsc_2 <- 126.6 # 2018
+
+fertile_tsc_1 <- 305.8 # 1977
+fertile_tsc_2 <- 296.1 # 2017
+
+# Reverse calculation with total
+infertile_tsc_t_1 <- (total_tsc_1-fertile_tsc_1*child1)/childless1
+infertile_tsc_t_2 <- (total_tsc_2-fertile_tsc_2*child2)/childless2
+
+# Reverse calculation with unselected
+infertile_tsc_u_1 <- (unselected_tsc_1-fertile_tsc_1*child1)/childless1
+infertile_tsc_u_2 <- (unselected_tsc_2-fertile_tsc_2*child2)/childless2
+
+### Negative predicted values
+infertile_sc_t_1
+infertile_sc_t_2
+infertile_sc_u_1
+infertile_sc_u_2
+
+infertile_tsc_t_1
+infertile_tsc_t_2
+infertile_tsc_u_1
+infertile_tsc_u_2
+
+### Results: Infertile men need to have a much steeper decline in quality
+# This compares infertile men with the reference from the paper
+1-infertile_sc_t_2/infertile_sc_t_1
+1-total_sc_2/total_sc_1 
+
+1-infertile_sc_u_2/infertile_sc_u_1
+1-unselected_sc_2/unselected_sc_1
+
+1-infertile_tsc_t_2/infertile_tsc_t_1
+1-total_tsc_2/total_tsc_1
+
+1-infertile_tsc_u_2/infertile_tsc_u_1
+1-unselected_tsc_2/unselected_tsc_1
---
+++ R/range.R
@@ -0,0 +1,34 @@
+# Range of proportions negatively selected
+childless1 <- seq(0.01,0.99,by=0.01)
+childless2 <- seq(0.01,0.99,by=0.01)
+child1 <- 1-childless1
+child2 <- 1-childless2
+nc1 <- length(childless1)
+nc2 <- length(childless2)
+
+# Matrix for results
+results <- matrix(data=NA,nrow=nc1,ncol=nc2)
+
+# Numbers from Human reproduction
+unselected_sc_1 <- 101.2 #
+unselected_sc_2 <- 49.0 # 2018
+
+fertile_sc_1 <- 77.3 # 1977
+fertile_sc_2 <- 72.8 # 2017
+
+# Values
+start <- (unselected_sc_1-fertile_sc_1*child1)/childless1
+last <- (unselected_sc_2-fertile_sc_2*child2)/childless2
+
+# Loop 
+for(i in 1:nc1) {
+  for(j in 1:nc2) {
+    if(start[i]<0 | last[j]<0) next
+    results[i,j] <- (last[i]-start[j])/start[j]
+  }
+}
+
+### Quick look at results
+image(t(results))
+# Negative selection has to be at least 30% for the second point in time
+# otherwise we get negative values
